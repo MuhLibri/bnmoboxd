d6ff8c65997ea758e0e93a2ee1b3e23c434411ee
chaerla <chelgchen@gmail.com>
2023-09-25 14:44:44 +0700

    feat: initialized index.php

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . '/../src/core/Application.php';
+require_once __DIR__ . '/../src/core/Router.php';
+require_once __DIR__ . '/../src/core/Request.php';
+require_once __DIR__ . '/../src/core/Response.php';
+require_once __DIR__ . '/../src/core/Controller.php';
+require_once __DIR__ . '/../src/controllers/FilmController.php';
+require_once __DIR__ . '/../src/db/Database.php';
+
+use app\core\Application;
+use app\controllers\FilmController;
+
+$config = [
+    'db' => [
+        'db_name' => $_ENV['MYSQL_DATABASE'],
+        'port' => $_ENV['MYSQL_PORT'],
+        'host' => $_ENV['MYSQL_HOST'],
+        'username' => $_ENV['MYSQL_USER'],
+        'password' => $_ENV['MYSQL_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
+$app->router->get('/', function() {
+    return 'Hello World';
+});
+$app->router->get("/test", function() {
+    return 'test';
+});
+$app->router->get('user/:id', function() {
+    return Application::$app->request->getParams()[0];
+});
+
+$app->router->get('/film/:id', [FilmController::class, 'index']);
+
+$app->run();
