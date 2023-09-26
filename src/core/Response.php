4c995c019c1483770f979f56e8635aee299fc713
chaerla <chelgchen@gmail.com>
2023-09-26 21:56:18 +0700

    fix: add jsonEncode and redirect method

--- src/core/Response.php
+++ src/core/Response.php
@@ -7,4 +7,14 @@ class Response
     public function setStatusCode(int $statusCode) {
         http_response_code($statusCode);
     }
+
+    public function jsonEncode($statusCode, $data) {
+        $this->setStatusCode($statusCode);
+        header('Content-Type: application/json');
+        return json_encode($data);
+    }
+
+    public function redirect(string $url) {
+        header("Location: $url");
+    }
 }
