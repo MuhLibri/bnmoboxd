4a54c71b2a3ee8aa7fb861096b6b321446ea9a9c
chaerla <chelgchen@gmail.com>
2023-09-28 05:01:28 +0700

    feat: add contents to index page

--- src/views/index.php
+++ src/views/index.php
@@ -1,1 +1,25 @@
-<?php include __DIR__ . '/components/navbar.php' ?>
+<?php
+    include __DIR__ . '/components/navbar.php';
+    include __DIR__ . '/components/dashboard/film-card.php';
+    include __DIR__ . '/components/dashboard/film-posters.php';
+?>
+<div class="dashboard-content">
+    <div>
+        <h5 class="section-title">MUST WATCH!</h5>
+        <div class="film-poster-list">
+            <?php
+            echo filmPosters($data["films"])
+            ?>
+        </div>
+    </div>
+    <div>
+        <h5 class="section-title">CHECK OUT THE LATEST REVIEWS!</h5>
+        <div class="review-list">
+            <?php
+            echo filmCard($data["filmReviews"])
+            ?>
+        </div>
+    </div>
+</div>
+
+
