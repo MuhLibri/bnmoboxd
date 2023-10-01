90d4256cd9f5f17923ae4426c4be164485823829
chaerla <chelgchen@gmail.com>
2023-10-02 00:40:48 +0700

    feat: add profile page (client)

---
+++ public/js/profile.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    handleFormSubmit("#profile-form", "/profile/edit", function (responseText) {
+        window.location.href = "/profile"
+    });
+
+    const saveButton = document.querySelector('#save-btn');
+    saveButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        handleOpen('#confirm-edit-modal');
+    });
+
+    const deleteButton = document.querySelector('#delete-btn');
+    deleteButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        handleOpen('#confirm-delete-modal');
+    });
+
+    const confirmEditButton = document.querySelector('#confirm-edit-btn');
+    confirmEditButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        const form = document.querySelector("#profile-form");
+        form.submit();
+        handleClose('#confirm-edit-modal');
+    });
+
+    const confirmDeleteButton = document.querySelector('#confirm-delete-btn');
+    confirmDeleteButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        const xhr = new XMLHttpRequest();
+        const url = '/profile/delete';
+        xhr.open('DELETE', url, true);
+        xhr.onreadystatechange = function () {
+            alert(xhr.responseText);
+            if (xhr.readyState === 4) { // Check if the request is complete
+                if (xhr.status === 200) {
+                    window.location.href = '/';
+                } else {
+                    alert('Failed to delete account');
+                }
+            }
+        };
+        xhr.send();
+    });
+});
---
+++ src/views/profile/index.php
@@ -0,0 +1,73 @@
+<?php
+
+use app\core\Application;
+
+include_once Application::$BASE_DIR . '/src/views/components/navbar.php';
+include_once Application::$BASE_DIR . '/src/views/components/confirmation-modal.php';
+
+$username = $data['profile']['username'];
+$email = $data['profile']['email'];
+$firstName = $data['profile']['firstName'];
+$lastName = $data['profile']['lastName'];
+$profilePicturePath = '/assets/users/' . $data['profile']['profilePicturePath'];
+$html = <<<EOT
+<div class="form-card">
+    <h2>Profile</h2>
+    <div class="user-profile">
+        <img src="$profilePicturePath">
+    </div>
+    <form class="form-container" id="profile-form">
+        <div class="form-group">
+            <label for="first_name">First Name</label>
+            <input type="text" id="first-name" name="first_name" formnovalidate value="$firstName">
+            <label class="form-error" id="first-name-form-error"></label>
+        </div>
+        <div class="form-group">
+            <label for="last_name">Last Name</label>
+            <input type="text" id="last_name" name="last_name" formnovalidate value="$lastName">
+            <label class="form-error" id="last-name-form-error"></label>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" formnovalidate value="$email">
+            <label class="form-error" id="email-form-error"></label>
+        </div>
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" formnovalidate value="$username">
+            <label class="form-error" id="username-form-error"></label>
+        </div>
+         <div class="form-group">
+            <label for="profile-picture">Profile Picture</label>
+            <input type="file" id="profile-picture" name="profile_picture" accept="image/*">
+        </div>
+        <div class="btn-group">
+            <button class="btn-primary" id="save-btn">Save Changes</button>
+            <button class = "btn-danger" id="delete-btn"">Delete Account</button>
+        </div>
+        <div class="modal-container" id="confirm-edit-modal">
+            <div class="confirmation-modal">
+                <h2>Are you sure you want to edit your profile?</h2>
+                <div class="btn-group">
+                    <button class="btn-primary" id="confirm-edit-btn">Yes</button>
+                    <button class="btn-danger" onclick="handleClose('#confirm-edit-modal')">Cancel</button>
+                </div>
+             </div>
+        </div>
+        <div class="modal-container" id="confirm-delete-modal">
+            <div class="confirmation-modal">
+                <h2>Are you sure you want to delete your profile?</h2>
+                <div class="btn-group">
+                    <button class="btn-primary" id="confirm-delete-btn">Yes</button>
+                    <button class="btn-danger" onclick="handleClose('#confirm-delete-modal')">Cancel</button>
+                </div>
+             </div>
+        </div>
+    </form>
+</div>
+<script defer src="/js/confirmation-modal.js"></script>   
+<script defer src="/js/profile.js"></script>
+<script defer src="/js/form-handler.js"></script>
+EOT;
+
+echo $html;
