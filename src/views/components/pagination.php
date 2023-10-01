8c8bcfc00386b1bb10a9e97216a4ed058585eced
chaerla <chelgchen@gmail.com>
2023-10-02 00:40:59 +0700

    feat: add pagination component

---
+++ src/views/components/pagination.php
@@ -0,0 +1,28 @@
+<?php
+
+$currentPage = $data['currentPage'];
+$buttonsToShow = 5;
+$totalPages = ceil($data['count'] / 10);
+$startPage = max(1, $currentPage - floor($buttonsToShow / 2));
+$endPage = min($totalPages, $startPage + 5 - 1);
+
+$str = '<div class="pagination-container">';
+
+if ($currentPage > 1) {
+    $prevPage = $currentPage - 1;
+    $str .= '<button class="pagination-button" onclick="handlePageChange(' . $prevPage . ')"> &larr; </button>';
+}
+
+for ($i = $startPage; $i <= $endPage; $i++) {
+    $buttonClass = $i == $currentPage ? 'pagination-button active' : 'pagination-button';
+    $str .= '<button class="' . $buttonClass . '" onclick="handlePageChange(' . $i . ')">' . $i . '</button>';
+}
+
+if ($currentPage < $endPage) {
+    $nextPage = $currentPage + 1;
+    $str .= '<button class="pagination-button" onclick="handlePageChange(' . $nextPage . ')"> &rarr; </button>';
+}
+
+$str .= '</div>';
+
+echo $str;
