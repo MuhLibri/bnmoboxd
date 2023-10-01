20d611507cf3728a0bd38f32c92fe075f9a18f01
chaerla <chelgchen@gmail.com>
2023-10-02 00:45:27 +0700

    feat: add profile controller

---
+++ src/controllers/ProfileController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Application;
+use app\core\Controller;
+use app\core\Request;
+use app\exceptions\BadRequestException;
+use app\exceptions\NotFoundException;
+use app\middlewares\AuthMiddleware;
+use app\services\UserService;
+
+class ProfileController extends Controller
+{
+    private UserService  $userService;
+
+    public function __construct()
+    {
+        require_once Application::$BASE_DIR . '/src/services/UserService.php';
+        require_once Application::$BASE_DIR . '/src/middlewares/AuthMiddleware.php';
+        $this->userService = new UserService();
+        $this->view = 'profile';
+        $this->middlewares = [
+          'index' => AuthMiddleware::class
+        ];
+    }
+
+    public function index() {
+        $username = $_SESSION['username'];
+        $user = $this->userService->getUserProfile($username);
+        $this->render('index', ['profile' => $user]);
+    }
+
+    /**
+     * @throws BadRequestException
+     * @throws NotFoundException
+     */
+    public function edit(Request $request) {
+        $data = $request->getBody();
+        $data['user_id'] = $_SESSION['user_id'];
+        $this->userService->updateUser($data);
+    }
+
+    public function delete() {
+        $userId = $_SESSION['user_id'];
+        $this->userService->deleteUser($userId);
+    }
+}
