729348c02bbe79766fe24317fa261acd96491d2e
chaerla <chelgchen@gmail.com>
2023-10-02 00:42:34 +0700

    feat: renamed authservice to userservice and add edit profile and delete account service

---
+++ src/services/UserService.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace app\services;
+
+use app\core\Application;
+use app\core\Service;
+use app\exceptions\BadRequestException;
+use app\exceptions\NotFoundException;
+use app\repositories\UserRepository;
+
+class UserService extends Service
+{
+    private UserRepository $userRepository;
+    public function __construct()
+    {
+        require_once Application::$BASE_DIR . '/src/repositories/UserRepository.php';
+        $this->userRepository = new UserRepository();
+    }
+
+    /**
+     * @throws BadRequestException
+     */
+    public function login($credentials) {
+        $errors = $this->validateRequired($credentials, ['username', 'password']);
+
+        $this->handleValidationErrors($errors);
+
+        $user = $this->userRepository->getUserByUsername($credentials['username']);
+
+        if ($user) {
+            $verify = password_verify($credentials['password'], $user['password_hash']);
+            if ($verify) {
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['role'] = $user['role'];
+                return;
+            }
+        }
+        $errors['auth'] = 'Invalid username/password';
+        $this->handleValidationErrors($errors);
+    }
+    /*
+     * Service level validation: checks for unique email and unique username
+     * Creates user if successful
+     * */
+    /**
+     * @throws BadRequestException
+     */
+    public function register(array $registerData) {
+        $this->validateRegisterFields($registerData);
+        $hashedPassword = $this->hashPassword($registerData['password']);
+        $this->userRepository->addUser($registerData['username'], $registerData['email'], $hashedPassword, $registerData['first_name'], $registerData['last_name']);
+    }
+
+    public function logout()
+    {
+        session_unset();
+        session_destroy();
+    }
+
+    public function getUserProfile($username): array {
+        $user = $this->userRepository->getUserByUsername($username);
+        return [
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'firstName' => $user['first_name'],
+            'lastName' => $user['last_name'],
+            'profilePicturePath' => $user['profile_picture_path']
+        ];
+    }
+    private function hashPassword (string $password) {
+        return password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    /**
+     * @throws BadRequestException
+     */
+    private function validateRegisterFields(array $data){
+        $errors = $this->validateRequired($data, ['username', 'password', 'first_name', 'email']);
+        $user = null;
+
+        if (!isset($errors['email'])) {
+            $errors = array_merge($errors, $this->validateEmail($data['email']));
+        }
+
+        if (!isset($errors['username'])) {
+            $errors = array_merge($errors, $this->validateUsername($data['username']));
+        }
+
+        $this->handleValidationErrors($errors);
+    }
+
+    /**
+     * @throws NotFoundException
+     * @throws BadRequestException
+     */
+    private function validateUpdateUserFields (array $updateData) {
+        $errors = $this->validateRequired($updateData, ['username', 'first_name', 'email']);
+        $currentUser = $this->userRepository->getUserById($updateData['user_id']);
+
+        if (!$currentUser) {
+            throw new NotFoundException(true);
+        }
+
+        if(!isset($errors['email'])) {
+            $errors = array_merge($errors, $this->validateEmail($updateData['email'], $currentUser));
+        }
+
+        if (!isset($errors['username'])) {
+            $errors = array_merge($errors, $this->validateUsername($updateData['username'], $currentUser));
+        }
+
+        $this->handleValidationErrors($errors);
+    }
+
+    private function validateEmail(string $email, $currentUser = null): array
+    {
+        $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+        $errors = [];
+        if (!preg_match($pattern, $email)) {
+            $errors['email'] = 'Email is invalid';
+        } else {
+            $userWithEmail = $this->userRepository->getUserByEmail($email);
+            if (($currentUser && $userWithEmail && $userWithEmail['id'] !== $currentUser['id']) || (!$currentUser && $userWithEmail)) {
+                $errors['email'] = 'Email is already taken';
+            }
+        }
+        return $errors;
+    }
+
+    private function validateUsername(string $username, $currentUser = null): array {
+        $pattern = "/^[a-zA-Z0-9_]{4,18}$/";
+        $errors = [];
+        if (!preg_match($pattern, $username)) {
+            $errors['username'] = 'Username should only contain letters (A-Z, a-z), numbers (0-9), and underscores (_) and must be 4 to 18 characters long.';
+        } else {
+            $userWithUsername = $this->userRepository->getUserByUsername($username);
+            if (($currentUser && $userWithUsername && $userWithUsername['id'] !== $currentUser['id']) || (!$currentUser && $userWithUsername)) {
+                $errors['username'] = 'Username is already taken';
+            }
+        }
+        return $errors;
+    }
+
+
+    /**
+     * @throws BadRequestException
+     * @throws NotFoundException
+     */
+    public function updateUser(array $updateData)
+    {
+        $this->validateUpdateUserFields($updateData);
+        $profilePicturePath = null;
+        if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['name'] !== '') {
+            $profilePicturePath = saveFile($_FILES['profile_picture'], Application::$BASE_DIR . '/public/assets/users/');
+        }
+        $this->userRepository->updateUser((int)$updateData['user_id'], $updateData['username'], $updateData['email'], $updateData['first_name'], $updateData['last_name'], $profilePicturePath);
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    public function deleteUser($userId)
+    {
+        $currentUser = $this->userRepository->getUserById($userId);
+
+        if (!$currentUser) {
+            throw new NotFoundException(true);
+        }
+        $this->userRepository->deleteUser((int)$userId);
+        $this->logout();
+    }
+}
