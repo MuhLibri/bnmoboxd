1085eeb1d7e88d8c1683ffc211a154d724d17b58
chaerla <chelgchen@gmail.com>
2023-10-04 23:57:33 +0700

    feat: add review list component

---
+++ src/views/components/review-list.php
@@ -0,0 +1,43 @@
+<?php
+
+use app\core\Application;
+
+$str = '';
+if(!empty($data['reviews'])){
+    $reviews = $data['reviews'];
+    foreach($reviews as $review) {
+        $name = $review['title'];
+        $id = $review['id'];
+        $filmPosterPath = '/assets/films/' . $review['image_path'];
+        $reviewText = $review['review'];
+        $rating = $review['rating'];
+        $dtCreate = new DateTime($review['updated_at'] ?? $review['created_at']);
+        $dateCreate = $dtCreate->format('M d, Y');
+
+        $starsHtml = str_repeat('<img src="/assets/app/star.png" alt="star" class="stars-img">', $rating);
+        $html = <<<EOT
+<a href="/my-reviews/$id" class="review-container" id="review-container-flex">                                                                                                                                     
+    <img src="$filmPosterPath" class="poster-image">
+    <div class="review-details">
+        <h6>
+            $name
+            <span class="review-date">
+                $dateCreate
+            </span>
+        </h6>
+        <div class="review-stars-container">$starsHtml</div>
+        <p>$reviewText</p>
+    </div>
+</a>
+EOT;
+        $str = $str . $html;
+    }
+    echo $str;
+}
+else {
+    echo '<p class="review-empty-text">No reviews.</p>';
+}
+
+if (isset($data['count']) && isset($data['currentPage'])) {
+    include Application::$BASE_DIR . '/src/views/components/pagination.php';
+}
