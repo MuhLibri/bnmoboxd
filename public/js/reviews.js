0e65e7fdbf6cdef5de978aeeeffa6a426a7f4bc1
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-10-04 21:17:11 +0700

    add modal to edit review

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -37,3 +37,54 @@
 //         review_box_container.appendChild(review);
 //     }
 // }
+
+
+// var save_review = document.getElementById("save");
+
+// save_review.addEventListener('click', function() {
+    
+// })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const saveButton = document.querySelector('#save');
+    saveButton.addEventListener('click', function (e) {
+        console.log("a")
+        e.preventDefault();
+        handleOpen('#confirm-edit-modal');
+    });
+
+    const deleteButton = document.querySelector('#delete');
+    deleteButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        handleOpen('#confirm-delete-modal');
+    });
+
+    const confirmEditButton = document.querySelector('#confirm-edit-btn');
+    confirmEditButton.addEventListener('click', function (e) {
+        console.log("b")
+        e.preventDefault();
+        // const form = document.querySelector("#profile-form");
+        // submitForm(form, "/profile/edit", function (responseText) {
+        //     window.location.href = "/profile"
+        // })
+        handleClose('#confirm-edit-modal');
+    });
+
+    const confirmDeleteButton = document.querySelector('#confirm-delete-btn');
+    confirmDeleteButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        // const xhr = new XMLHttpRequest();
+        // const url = '/profile/delete';
+        // xhr.open('DELETE', url, true);
+        // xhr.onreadystatechange = function () {
+        //     if (xhr.readyState === 4) { // Check if the request is complete
+        //         if (xhr.status === 200) {
+        //             window.location.href = '/';
+        //         } else {
+        //             alert('Failed to delete account');
+        //         }
+        //     }
+        // };
+        // xhr.send();
+    });
+});
--- src/views/reviews/edit.php
+++ src/views/reviews/edit.php
@@ -26,19 +26,46 @@ function showReview($review){
     $starsHtml = str_repeat('<img src="/assets/app/star.png" alt="star" class="stars-img">', $rating);
     $html = <<<"EOT"
         <div class="edit-review-container">
-            <div class="film-poster">
-                <img src="$profilePicturePath">
-            </div>
-            <div class="review-details">
+            <div class="review-upper-section">
+                <img class="film-poster-edit" src="$profilePicturePath">
+                <div class="review-details">
+                <h6 class="film-title" id="ftr1">$name</h6>
                 <h6>
-                    $name
                     <span class="review-date">
                         $dateCreate
                         $dateUpdate
                     </span>
                 </h6>
-                <div class="review-stars-container">$starsHtml</div>
-                <p>$reviewText</p>
+                <div class="edit-star">
+                    <input type="radio" name="star" id="star1" value="1">
+                    <label class="star-label" for="star1">
+                        <div class="review-stars-container">$starsHtml 1</div>
+                    </label>
+
+                    <input type="radio" name="star" id="star2" value="2">
+                    <label class="star-label" for="star2">
+                        <div class="review-stars-container">$starsHtml 2</div>
+                    </label>
+
+                    <input type="radio" name="star" id="star3" value="3">
+                    <label class="star-label" for="star3">
+                        <div class="review-stars-container">$starsHtml 3</div>
+                    </label>
+
+                    <input type="radio" name="star" id="star4" value="4">
+                    <label class="star-label" for="star4">
+                        <div class="review-stars-container">$starsHtml 4</div>                    
+                    </label>
+
+                    <input type="radio" name="star" id="star5" value="5" checked>
+                    <label class="star-label" for="star5">
+                        <div class="review-stars-container">$starsHtml 5</div>
+                    </label>
+                </div>
+                <div class="edit-text-box">
+                    <textarea class="edit-textarea">$reviewText</textarea>
+                </div>
+            </div>
             </div>
         </div>
     EOT;
@@ -54,12 +81,34 @@ function showReview($review){
             <?php echo showReview(1) ?>
         </div>
         <div class="review-button-container">
-            <input type="image" class="review-button" src="/assets/app/edit.png" name="Edit"/>
-            <input type="image" class="review-button" src="/assets/app/delete.png" name="Edit"/>
+            <input type="image" class="review-button" id="save" src="/assets/app/save-review.png" name="Save"/>
+            <input type="image" class="review-button" id="delete" src="/assets/app/delete.png" name="Delete"/>
             <!-- <button src="/assets/app/edit.png" class="review-button">Edit</button> -->
             <!-- <button class="review-button">Delete</button> -->
         </div>
     </div>
 </div>
 
+<div class="modal-container" id="confirm-edit-modal">
+    <div class="confirmation-modal">
+        <h2>Are you sure you want to edit your review?</h2>
+        <div class="btn-group">
+            <button class="btn-primary" id="confirm-edit-btn">Yes</button>
+            <button class="btn-danger" onclick="handleClose('#confirm-edit-modal')">Cancel</button>
+        </div>
+    </div>
+</div>
+
+<div class="modal-container" id="confirm-delete-modal">
+    <div class="confirmation-modal">
+        <h2>Are you sure you want to delete your review?</h2>
+        <div class="btn-group">
+            <button class="btn-primary" id="confirm-delete-btn">Yes</button>
+            <button class="btn-danger" onclick="handleClose('#confirm-delete-modal')">Cancel</button>
+        </div>
+    </div>
+</div>
+
 <script defer src="/js/reviews.js"></script>
+<script defer src="/js/confirmation-modal.js"></script>   
+<script defer src="/js/form-handler.js"></script>
