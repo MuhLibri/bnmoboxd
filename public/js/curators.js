cadc446054eae28e9aa24292b31dc910cadc8b28
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-10-31 21:34:32 +0700

    add pagination to curator reviews

---
+++ public/js/curators.js
@@ -0,0 +1,16 @@
+function sendRequest(query) {
+    const xhr = new XMLHttpRequest();
+    const url = `/my-reviews/search?${query}`;
+    xhr.open('GET', url);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById('rl1').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+function handlePageChange(page) {
+    const query = `page=${page}&take=5`;
+    sendRequest(query);
+}
--- src/views/curators/show.php
+++ src/views/curators/show.php
@@ -35,42 +35,45 @@ function showCuratorReviews($data) {
     $str = "";
     $subscribed = $data['status'] == 'ACCEPTED';
     if ($subscribed) {
-        $reviews = $data['reviews'];
-        foreach($reviews as $review) {
-            $name = $review['title'];
-            $id = $review['id'];
-            $filmPosterPath = '/assets/films/' . $review['image_path'];
-            $reviewText = $review['review'];
-            $rating = $review['rating'];
-            $dtCreate = new DateTime($review['created_at']);
-            $dtUpdate = new DateTime($review['updated_at']);
-            $dateCreate = $dtCreate->format('M d, Y');
-            $dateUpdate = $dtCreate != $dtUpdate ? ' • Updated on ' . $dtUpdate->format('M d, Y') : '';
-
-            $starsHtml = str_repeat('<img src="/assets/app/star.png" alt="star" class="stars-img">', $rating);
-            $html = <<<EOT
-            <div class="review-container" id="review-container-flex">                                                                                                                                     
-                <img alt="film poster" src="$filmPosterPath" class="poster-image">
-                <div class="review-details">
-                    <h6>
-                        $name
-                        <span class="review-date">
-                            $dateCreate
-                            $dateUpdate
-                        </span>
-                    </h6>
-                    <div class="review-stars-container">$starsHtml</div>
-                    <p>$reviewText</p>
+        if (!empty($data['reviews'])) {
+            $reviews = $data['reviews'];
+            foreach($reviews as $review) {
+                $name = $review['title'];
+                $id = $review['id'];
+                $filmPosterPath = '/assets/films/' . $review['image_path'];
+                $reviewText = $review['review'];
+                $rating = $review['rating'];
+                $dtCreate = new DateTime($review['created_at']);
+                $dtUpdate = new DateTime($review['updated_at']);
+                $dateCreate = $dtCreate->format('M d, Y');
+                $dateUpdate = $dtCreate != $dtUpdate ? ' • Updated on ' . $dtUpdate->format('M d, Y') : '';
+    
+                $starsHtml = str_repeat('<img src="/assets/app/star.png" alt="star" class="stars-img">', $rating);
+                $html = <<<EOT
+                <div class="review-container" id="review-container-flex">                                                                                                                                     
+                    <img alt="film poster" src="$filmPosterPath" class="poster-image">
+                    <div class="review-details">
+                        <h6>
+                            $name
+                            <span class="review-date">
+                                $dateCreate
+                                $dateUpdate
+                            </span>
+                        </h6>
+                        <div class="review-stars-container">$starsHtml</div>
+                        <p>$reviewText</p>
+                    </div>
                 </div>
-            </div>
-            EOT;
-            $str = $str . $html;
+                EOT;
+                $str = $str . $html;
+            }
+        }
+        else {
+            $str = '<p class="empty-text">No reviews.</p>';
         }
     }
     else {
-        $str = <<<EOT
-            <h6>You have not subscribed</h6>
-        EOT;
+        $str = '<h6>You have not subscribed</h6>';
     }
 
     return $str;
@@ -87,8 +90,12 @@ function showCuratorReviews($data) {
     <div class="review-list" id="rl1">
         <?php
             echo showCuratorReviews($data);
+            if (isset($data['count']) && isset($data['currentPage'])) {
+                include Application::$BASE_DIR . '/src/views/components/pagination.php';
+            }
         ?>
     </div>
 </div>
 
-<script defer src="/js/curators.js"></script>
+<!-- <script defer src="/js/curators.js"></script> -->
+<script defer src="/js/reviews.js"></script>
