3ff92088ab9fd090fb4d5a9713a96e4ce9b1a12f
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-11-04 17:11:59 +0700

    fix CuratorsRepository

--- migrations/init.sql
+++ migrations/init.sql
@@ -183,5 +183,6 @@ INSERT INTO subscriptions (curator_id, subscriber_id, status)
 VALUES
     (1, 1, 'ACCEPTED'),
     (1, 2, 'ACCEPTED'),
+    (1, 3, 'PENDING'),
     (2, 1, 'PENDING'),
     (3, 1, 'REJECTED');
