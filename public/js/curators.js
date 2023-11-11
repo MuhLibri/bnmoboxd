bc8047cea6cbaaf17e2457942bc4cda8d953e023
chaerla <chelgchen@gmail.com>
2023-11-11 19:14:32 +0700

    feat: curators js and styling

--- public/js/curators.js
+++ public/js/curators.js
@@ -1,10 +1,10 @@
 function sendRequest(query) {
     const xhr = new XMLHttpRequest();
-    const url = `/my-reviews/search?${query}`;
+    const url = `/curators/search?${query}`;
     xhr.open('GET', url);
     xhr.onload = function () {
         if (xhr.status === 200) {
-            document.getElementById('rl1').innerHTML = xhr.responseText;
+            document.getElementById('cl').innerHTML = xhr.responseText;
         }
     };
     xhr.send();
@@ -13,6 +13,20 @@ function sendRequest(query) {
 function handlePageChange(page) {
     const query = `page=${page}&take=5`;
     sendRequest(query);
+}
+
+function subscribe() {
+    const xhr = new XMLHttpRequest();
+    const curatorUsername = document.getElementById('curator_username').value;
+    xhr.open("post", `/curators/${curatorUsername}/subscribe`, true);
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+        if (xhr.status === 200) {
+            window.location.href = `/curators/${curatorUsername}`;
+        }
+    };
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -24,30 +38,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const confirmEditButton = document.querySelector('#confirm-subscribe-btn');
     confirmEditButton?.addEventListener('click', function (e) {
-        console.log("a");
         e.preventDefault();
-        // Handle subcribe/unsubscribe
-        
-        // const form = document.querySelector("#review-form");
-        // submitForm(form, window.location.href, function (responseText) {
-        //     window.location.href = "/curators/:id";
-        // })
+        subscribe();
         handleClose('#confirm-subscribe-modal');
     });
 });
-
-const subscription_status = document.querySelectorAll('.status-text');
-subscription_status.forEach(function coloring(ss) {
-    if (ss.innerHTML == "Accepted") {
-        ss.style.backgroundColor = "green";
-    }
-    else if (ss.innerHTML == "Pending") {
-        ss.style.backgroundColor = "#F29339";
-    }
-    else if (ss.innerHTML == "Rejected") {
-        ss.style.backgroundColor = "red";
-    }
-    else {
-        ss.style.backgroundColor = "grey";
-    }
-})
